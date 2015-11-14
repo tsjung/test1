d7c6ebee43b58373bece53668c8bc7fd3d8313ed
kevin <kevin@DESKTOP-4H8N09T>
2015-11-14 19:15:29 +0900

    helloworld

---
+++ test1/src/test/java/test1/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package test1;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class HelloWorldTest extends TestCase {
+
+	@Test
+	public void test() {
+		System.out.println("hello world!!");
+	}
+}
